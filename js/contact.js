9c77e29b7271b038c2a6cb94ed6f3138f4da8df6
lMartin3 <me@martinl.dev>
2025-07-31 15:37:45 -0300

    create contact page

---
+++ js/contact.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function () {
+    displayScores('date');
+});
+
+function displayScores(sortBy) {
+    var scores = readScores();
+    var sortFunction = null;
+    switch (sortBy) {
+        case 'date':
+            sortFunction = function (a, b) {
+                return a.timestamp - b.timestamp;
+            }
+            break;
+        default:
+        case 'time':
+            sortFunction = function (a, b) {
+                return a.time - b.time;
+            }
+            break;
+    }
+    scores.sort(sortFunction)
+    var scoreboard = document.getElementById("scoreboard-body");
+    scoreboard.innerHTML = "";
+    var addedScores = 0;
+    for(var score of scores) {
+        var row = document.createElement("tr");
+        row.innerHTML = `<td class="left">${score.name}</td><td class="center">${score.difficulty}</td><td class="center">${score.time}s</td><td class="right">${new Date(score.timestamp).toLocaleDateString('en-GB')}</td>`;
+        scoreboard.appendChild(row);
+        addedScores++;
+        if(addedScores >= 15) break;
+    }
+}
